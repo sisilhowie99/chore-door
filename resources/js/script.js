550bc427839f4ca01994bc4f27f62b9e3f42e340
sisilhowie99 <sisil.anastasia@gmail.com>
2020-05-24 19:43:55 +1000

    added doors and images; overall design complete

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,14 @@
+//door images
 let doorImage1 = document.getElementById('door1');
+let doorImage2 = document.getElementById('door2');
+let doorImage3 = document.getElementById('door3');
+
+//images inside the door 
 let botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
+let beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
+let spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
 
 //when doorImage is clicked to open the door
 doorImage1.onclick = () => {doorImage1.src = botDoorPath};
+doorImage2.onclick = () => {doorImage2.src = beachDoorPath};
+doorImage3.onclick = () => {doorImage3.src = spaceDoorPath};
